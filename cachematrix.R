7fdd923278f5353314d25a76f7068ed5a6da218b
matenduel <31441909+matenduel@users.noreply.github.com>
2019-12-06 03:03:15 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  # Initializing
+  inv <- NULL
+        
+  # make Methods
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+        
+  # return list
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 
 }
 
@@ -11,5 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+   # check exist of inv
+  if(!is.null(inv)) {
+    message("getting cached result")
+    return(inv)
+  } else {
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  return(inv)
+  }
 }
